8fb8d495eb86562d9f6b66151f6e901e75659451
David Collins <dcollins@mullaghmore.local>
2019-10-14 20:13:45 -0400

    Refactor a bit.  Add derived quantities

--- tools_data/data_puller.py
+++ tools_data/data_puller.py
@@ -4,14 +4,26 @@ all_nonzero = looper.get_all_nonzero()
 #many1: frame_list = list(range(10,130,10)) + [125]
 #many1: core_list =  [ 120, 10, 308, 41, 44, 110, 1, 65], 
 #frame 120 core 96
-core_list = all_nonzero.astype('int')[-3:]
-frame_list = [1,125]
-fields = ['x','y','z','velocity_magnitude','magnetic_field_strength', 'velocity_divergence']
-output_base = "some_track_set"
-#core_list=[31]
-for core in core_list:
+if 0:
+    core_list = all_nonzero.astype('int')
+    frame_list = [0,1]+list(range(10,130,10))+[125]
+    fields = ['x','y','z','velocity_magnitude','magnetic_field_strength', 'velocity_divergence']
+    fields += ['velocity_x','velocity_y','velocity_z']
+    fields += ['magnetic_field_%s'%s for s in 'xyz']
+    output_base = "all_primitives"
+    derived=[]
+if 1:
+    import xtra_energy
+    core_list = all_nonzero.astype('int')
+    frame_list = [0,1]+list(range(10,130,50))+[125]
+    fields = ['mag_work']
+    derived=[xtra_energy.add_force_terms]
+    output_base = 'mag_work_tmp'
+for core in core_list:  
+    print("core %d"%core)
     output_name = '%s_c%04d.h5'%(output_base,core)
     if os.path.exists(output_name):
+        print("File exists, skipping "+output_name)
         continue
     this_looper = looper.core_looper(directory= dl.enzo_directory,
                                      sim_name = 'u05',
@@ -20,6 +32,7 @@ for core in core_list:
                                      frame_list = frame_list,
                                      core_list =  [core],# core_list,
                                      fields_from_grid=fields,
+                                     derived = derived
                                   )
     this_looper.get_target_indices(h5_name='u05_0125_peaklist.h5',
                                      bad_particle_list='bad_particles.h5')

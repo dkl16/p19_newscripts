da57583460f401e4bbc8f404200a933cbfc08a4e
dcollins4096 <dcollins4096@gmail.com>
2019-10-14 21:44:12 -0400

    starter2 order of paths

--- starter2.py
+++ starter2.py
@@ -4,6 +4,13 @@
 #
 
 from starter1 import *
+#so we can import things from sub directories
+path_list= ["./data_tools", "./pdf_tools", "./testing",\
+            "./tools", "./track_tools", "./trash"]
+for directory in path_list:
+    if directory not in sys.path:
+        sys.path += [directory]
+
 import yt
 import looper
 reload(looper)
@@ -20,11 +27,5 @@ import tracks_read_write as trw
 reload(trw)
 from davetools import *
 
-#so we can import things from sub directories
-path_list= ["./data_tools", "./pdf_tools", "./testing",\
-            "./tools", "./track_tools", "./trash"]
-for directory in path_list:
-    if directory not in os.path:
-        os.path += [directory]
 
 

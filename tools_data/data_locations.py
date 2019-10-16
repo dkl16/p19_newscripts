d548758b8173cf2e527c6736bcc6b7b691b8a8a0
dcollins4096 <dcollins4096@gmail.com>
2019-10-16 09:39:36 -0400

    generalize data_locations

--- tools_data/data_locations.py
+++ tools_data/data_locations.py
@@ -1,6 +1,13 @@
+"""
+A container for code portability.
+Put
+export machine = my_machine_name
+in your .bashrc
+Shipsterns and Mullaghmore are two of my computers, copy that.
+"""
 from starter2 import *
 enzo_directory = '/scratch2/dcollins/Paper19_48/B02/u05-r4-l4-128/GravPotential'
-snapshot_location ='/scratch1/dcollins/Paper19/Datasets/track_indfix_sixteenframe'
+sixteen_frame ='/scratch1/dcollins/Paper19/Datasets/track_indfix_sixteenframe'
 snapshot_base ='/scratch1/dcollins/Paper19/Datasets/'
 if 'machine' in os.environ:
     if os.environ['machine'] in ['shipsterns']:

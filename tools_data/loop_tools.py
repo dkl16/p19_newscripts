609267a1426d1c5e1e3e234a83e27f1b3c0c08cc
dcollins4096 <dcollins4096@gmail.com>
2019-10-18 09:15:59 -0400

    update particle callback

--- tools_data/loop_tools.py
+++ tools_data/loop_tools.py
@@ -184,30 +184,23 @@ def deposit_target_particles_1(field, data):
     data.set_field_parameter('timer',t0+[t1])
     return data.apply_units(d, field.units)
 
-#yt.add_field(      ("deposit","deposit_target_particles"),
-##yt.add_field(      'dp1',
-#         function = deposit_target_particles_1,
-#         validators = [yt.ValidateSpatial(), 
-#                       yt.ValidateParameter('indices_late'), 
-#                       yt.ValidateParameter('mask_to_get'), 
-#                       yt.ValidateGridType()],
-#         display_name = "target_particles",sampling_type='cell')
 
-class SelectParticleCallback(PlotCallback):
+class NewSelectParticleCallback(PlotCallback):
+    """
+    Adds particle positions, based on a thick slab along *axis* with a
+    *width* along the line of sight.  *p_size* controls the number of
+    pixels per particle, and *col* governs the color.  *ptype* will
+    restrict plotted particles to only those that are of a given type.
+    *alpha* determines the opacity of the marker symbol used in the scatter.
+    An alternate data source can be specified with *data_source*, but by
+    default the plot's data source will be queried.
+    """
     _type_name = "select_particles"
     region = None
     _descriptor = None
-    def __init__(self, width, p_size=1.0, col='k', marker='o', stride=1,
-                 ptype='all', stars_only=False, dm_only=False,
-                 minimum_mass=None,bool=None, indices=None, xyz=None):
-        """
-        Adds particle positions, based on a thick slab along *axis* with a
-        *width* along the line of sight.  *p_size* controls the number of
-        pixels per particle, and *col* governs the color.  *ptype* will
-        restrict plotted particles to only those that are of a given type.
-        *minimum_mass* will require that the particles be of a given mass,
-        calculated via ParticleMassMsun, to be plotted.
-        """
+    _supported_geometries = ("cartesian", "spectral_cube", "cylindrical-2d")
+    def __init__(self, width,indices=None, p_size=1.0, col='k', marker='o', stride=1,
+                 ptype='all', minimum_mass=None, alpha=1.0, data_source=None):
         PlotCallback.__init__(self)
         self.width = width
         self.p_size = p_size
@@ -215,112 +208,111 @@ class SelectParticleCallback(PlotCallback):
         self.marker = marker
         self.stride = stride
         self.ptype = ptype
-        self.stars_only = stars_only
-        self.dm_only = dm_only
         self.minimum_mass = minimum_mass
-        self.bool = bool
-        self.indices = indices
-        self.xyz = xyz
+        self.alpha = alpha
+        self.data_source=data_source
+        self.indices=indices
+        if self.minimum_mass is not None:
+            warnings.warn("The minimum_mass keyword is deprecated.  Please use "
+                          "an appropriate particle filter and the ptype keyword instead.")
+
 
     def __call__(self, plot):
         data = plot.data
         if iterable(self.width):
-            self.width = np.float64(plot.data.ds.quan(self.width[0], self.width[1]))
-        # we construct a recantangular prism
-        x0, x1 = plot.xlim
-        y0, y1 = plot.ylim
+            validate_width_tuple(self.width)
+            self.width = plot.data.ds.quan(self.width[0], self.width[1])
+        elif isinstance(self.width, YTQuantity):
+            self.width = plot.data.ds.quan(self.width.value, self.width.units)
+        else:
+            self.width = plot.data.ds.quan(self.width, "code_length")
+        # we construct a rectangular prism
+        x0 = plot.xlim[0].to("code_length")
+        x1 = plot.xlim[1].to("code_length")
+        y0 = plot.ylim[0].to("code_length")
+        y1 = plot.ylim[1].to("code_length")
         xx0, xx1 = plot._axes.get_xlim()
         yy0, yy1 = plot._axes.get_ylim()
-        if self.bool is None:
-            reg = self._get_region((x0,x1), (y0,y1), plot.data.axis, data)
+        if type(self.data_source)==YTCutRegion:
+            mylog.warn("Parameter 'width' is ignored in annotate_particles if the "
+                       "data_source is a cut_region. "
+                       "See https://github.com/yt-project/yt/issues/1933 for further details.")
+            self.region=self.data_source
         else:
-            reg = self.bool
+            self.region=self._get_region((x0,x1), (y0,y1), plot.data.axis, data)
         ax = data.axis
-
-
         xax = plot.data.ds.coordinates.x_axis[ax]
         yax = plot.data.ds.coordinates.y_axis[ax]
         axis_names = plot.data.ds.coordinates.axis_name
         field_x = "particle_position_%s" % axis_names[xax]
         field_y = "particle_position_%s" % axis_names[yax]
         pt = self.ptype
-        shifted_field_x = reg[pt, field_x]
-        shifted_field_y = reg[pt, field_y]
-        shifted_field = [shifted_field_x, shifted_field_y]
-        lower_lim = data.ds.arr([xx0,yy0],'code_length')
-        upper_lim = data.ds.arr([xx1,yy1],'code_length')
-        dx_min = data.ds.index.get_smallest_dx()*0.1
-
-        for ndim,dim in enumerate([xax,yax]):
-            if data.ds.periodicity[dim] and True:
-                #print("SHIFT", ndim, dim)
-                #If the plot is to the right of the domain
-                delta = upper_lim[ndim] - data.ds.domain_right_edge[dim]
-                #print("DELTA up", dim, delta, upper_lim[ndim])
-                if delta > dx_min:
-                    delta = delta + data.ds.domain_left_edge[dim]
-                    particles_to_shift = np.where(shifted_field[ndim] < delta)
-                    shifted_field[ndim][particles_to_shift] += data.ds.domain_width[dim]
-                #If the plot is to the left
-                delta = data.ds.domain_left_edge[dim] - lower_lim[ndim]
-                if delta > dx_min:
-                    delta = data.ds.domain_right_edge[dim] - delta
-                    particles_to_shift = np.where(shifted_field[ndim] > delta)
-                    shifted_field[ndim][particles_to_shift] -= data.ds.domain_width[dim]
-
-
-
-        gg = ( ( shifted_field_x >= x0 ) & ( shifted_field_x <= x1 )
-           &   ( shifted_field_y >= y0 ) & ( shifted_field_y <= y1 ) )
-        #print("CHECK", x0, x1, y0, y1, "npart", reg['particle_index'].size,"n(gg)", gg.sum())
-        #print("CHECK", shifted_field_x.min(), shifted_field_x.max(), shifted_field_y.min(),shifted_field_y.max())
-        #import pdb
-        #pdb.set_trace()
-
+        self.periodic_x = plot.data.ds.periodicity[xax]
+        self.periodic_y = plot.data.ds.periodicity[yax]
+        self.LE = plot.data.ds.domain_left_edge[xax], \
+                  plot.data.ds.domain_left_edge[yax]
+        self.RE = plot.data.ds.domain_right_edge[xax], \
+                  plot.data.ds.domain_right_edge[yax]
+        period_x = plot.data.ds.domain_width[xax]
+        period_y = plot.data.ds.domain_width[yax]
+        particle_x, particle_y = self._enforce_periodic(self.region[pt, field_x],
+                                                        self.region[pt, field_y],
+                                                        x0, x1, period_x,
+                                                        y0, y1, period_y)
+        gg = ( ( particle_x >= x0 ) & ( particle_x <= x1 )
+           &   ( particle_y >= y0 ) & ( particle_y <= y1 ) )
         if self.indices is not None:
             mask_to_get = na.zeros(self.indices.shape, dtype='int32')
             found_any, mask = particle_ops.mask_particles(
-                self.indices.astype('int64'), reg['particle_index'].astype('int64'), mask_to_get)
+                self.indices.astype('int64'), self.region['particle_index'].astype('int64'), mask_to_get)
             gg = ( gg & (mask == 1) )
 
         print( "nparticles in particle callback.", mask_to_get.sum())
-        if False:
-            if self.ptype is not None:
-                gg &= (reg["particle_type"] == self.ptype)
-                if gg.sum() == 0: return
-            if self.stars_only:
-                gg &= (reg["creation_time"] > 0.0)
-                if gg.sum() == 0: return
-            if self.dm_only:
-                gg &= (reg["creation_time"] <= 0.0)
-                if gg.sum() == 0: return
-            if self.minimum_mass is not None:
-                gg &= (reg["ParticleMassMsun"] >= self.minimum_mass)
-                if gg.sum() == 0: return
-        #plot._axes.hold(True)
-        px, py = self.convert_to_plot(plot,
-                    [np.array(shifted_field_x[gg][::self.stride]),
-                     np.array(shifted_field_y[gg][::self.stride])])
+        if self.minimum_mass is not None:
+            gg &= (self.region[pt, "particle_mass"] >= self.minimum_mass)
+            if gg.sum() == 0: return
+        px, py = [particle_x[gg][::self.stride], particle_y[gg][::self.stride]]
+        px, py = self._convert_to_plot(plot, [px, py])
         plot._axes.scatter(px, py, edgecolors='None', marker=self.marker,
-                           s=self.p_size, c=self.color)
+                           s=self.p_size, c=self.color,alpha=self.alpha)
         plot._axes.set_xlim(xx0,xx1)
         plot._axes.set_ylim(yy0,yy1)
-        #plot._axes.hold(False)
+
+    def _enforce_periodic(self,
+                          particle_x,
+                          particle_y,
+                          x0, x1, period_x,
+                          y0, y1, period_y):
+        #  duplicate particles if periodic in that direction AND if the plot
+        #  extends outside the domain boundaries.
+        if self.periodic_x and x0 > self.RE[0]:
+            particle_x = uhstack((particle_x, particle_x + period_x))
+            particle_y = uhstack((particle_y, particle_y))
+        if self.periodic_x and x1 < self.LE[0]:
+            particle_x = uhstack((particle_x, particle_x - period_x))
+            particle_y = uhstack((particle_y, particle_y))
+        if self.periodic_y and y0 > self.RE[1]:
+            particle_y = uhstack((particle_y, particle_y + period_y))
+            particle_x = uhstack((particle_x, particle_x))
+        if self.periodic_y and y1 < self.LE[1]:
+            particle_y = uhstack((particle_y, particle_y - period_y))
+            particle_x = uhstack((particle_x, particle_x))
+        return particle_x, particle_y
 
     def _get_region(self, xlim, ylim, axis, data):
         LE, RE = [None]*3, [None]*3
-        ds=data.ds
+        ds = data.ds
         xax = ds.coordinates.x_axis[axis]
         yax = ds.coordinates.y_axis[axis]
         zax = axis
         LE[xax], RE[xax] = xlim
         LE[yax], RE[yax] = ylim
-        LE[zax] = data.center[zax].ndarray_view()- self.width*0.5
-        RE[zax] = data.center[zax].ndarray_view()+ self.width*0.5
+        LE[zax] = data.center[zax] - self.width*0.5
+        LE[zax].convert_to_units("code_length")
+        RE[zax] = LE[zax] + self.width
         if self.region is not None \
-            and na.all(self.region.left_edge <= LE) \
-            and na.all(self.region.right_edge >= RE):
+            and np.all(self.region.left_edge <= LE) \
+            and np.all(self.region.right_edge >= RE):
             return self.region
-        self.region = data.ds.region( data.center, LE, RE)
+        self.region = data.ds.region(data.center, LE, RE, data_source=self.data_source)
         return self.region
-

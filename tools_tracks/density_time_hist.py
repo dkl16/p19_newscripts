f54433ce3803c970ce3dcf021bd620f3008ba342
dcollins4096 <dcollins4096@gmail.com>
2019-10-21 14:05:58 -0400

    density histogram

--- tools_tracks/density_time_hist.py
+++ tools_tracks/density_time_hist.py
@@ -1,7 +1,38 @@
 import matplotlib.colors as colors
-if 0:
-    nbins=100
-    thhist = np.zeros([nbins,len(asort)])
+
+from starter2 import *
+import data_locations as dl
+reload(dl)
+plt.close('all')
+
+file_list=glob.glob('%s/*h5'%dl.sixteen_frame)
+out_prefix = 'CHANGE_THIS_PREFIX'
+
+out_prefix='test'
+file_list = file_list[:2]
+
+
+if 'this_looper' not in dir():
+    this_looper=looper.core_looper(directory=dl.enzo_directory)
+    for nfile,fname in enumerate(file_list):
+        this_looper.load_loop(fname)
+        print( "File %d of %d"%(nfile,len(file_list)))
+    thtr = this_looper.tr
+    thtr.sort_time()
+all_cores = np.unique(thtr.core_ids)
+rm = rainbow_map(len(all_cores))
+
+core_list = all_cores
+fig, ax= plt.subplots(1,1)
+nbins=100
+for core_id in core_list:
+
+    ax.clear()
+    thhist = np.zeros([nbins,len(thtr.times)])
+    ms = trackage.mini_scrubber(thtr,core_id)
+    density = thtr.c([core_id],'density')
+    tmap=rainbow_map(ms.ntimes)
+    asort =  np.argsort(thtr.times)
     mind = density.min()
     maxd = density.max()
     bins = np.logspace(np.log10(mind), np.log10(maxd),nbins+1)
@@ -15,8 +46,10 @@ if 0:
     cbar.set_clim(vmin=1,vmax=thhist.max())
     cbar.cmap.set_under('w')
 
-    fig.savefig('density_time_hist.png')
-if 1:
+    outname = '%s/density_time_hist_c%04d.png'%(dl.output_directory,core_id)
+    fig.savefig(outname )
+    print("wrote "+outname)
+if 0:
     fig2,ax2=plt.subplots(1,1)
     nparticles = density.shape[0]
     times_ordered=thtr.times[asort]

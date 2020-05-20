b3b540cde41f15f381af7bdd1af2f568aed37ee0
dcollins4096 <dcollins4096@gmail.com>
2020-05-20 12:40:07 -0400

    minor clean up.  What was the dv business?

--- tools_tracks/image_tracks.py
+++ tools_tracks/image_tracks.py
@@ -12,6 +12,10 @@ out_prefix = 'CHANGE_THIS_PREFIX'
 out_prefix='test'
 file_list = file_list[:2]
 
+import tools_tracks.alpha_properties as ap 
+this_looper=ap.this_looper
+thtr=this_looper.tr
+thtr.sort_time()
 
 if 'this_looper' not in dir():
     this_looper=looper.core_looper(directory=dl.enzo_directory)
@@ -23,17 +27,35 @@ if 'this_looper' not in dir():
 all_cores = np.unique(thtr.core_ids)
 rm = rainbow_map(len(all_cores))
 
-core_list = all_cores
+core_list = [14] #all_cores
 fig_many, ax_many = plt.subplots(1,1)
 for core_id in core_list:
     ms = trackage.mini_scrubber(thtr,core_id)
     tmap=rainbow_map(ms.ntimes)
     asort =  np.argsort(thtr.times)
     delta=0.1
+
+    rrr = ms.r[:,asort]
+    dv = rrr[:,1:] - rrr[:,:-1]
+    mask = dv[:,1]*dv[:,2] < 0
     for it,nt in enumerate(asort):
+        if it < 2:
+            continue
+        nt0 = asort[it-1]
+        ntm2 = asort[it-2]
+        d2v = (rrr[:,nt] - rrr[:,nt0])*(rrr[:,nt0]-rrr[:,ntm2])
+        mask = d2v<0
+
+        print("NNNN",(d2v < 0).sum())
+
+
         ax_many.clear()
         ax_many.plot([0,1,1,0,0],[0,0,1,1,0])
-        ax_many.scatter(ms.this_x[:,nt],ms.this_y[:,nt],s=0.1)
+        this_frame = sorted(thtr.frames)[nt]
+        cen=this_looper.snaps[this_frame][core_id].R_centroid
+        cenx = cen[0]; ceny=cen[1]
+        ax_many.scatter(ms.this_x[mask,nt],ms.this_y[mask,nt],s=0.1)
+        ax_many.scatter( cenx,ceny,c='k',s=3)
         ax_many.set_xlim(-delta,1+delta); ax_many.set_ylim(-delta,1+delta)
         title='t=%0.5f'%thtr.times[nt]
         ax_many.set_title(title)

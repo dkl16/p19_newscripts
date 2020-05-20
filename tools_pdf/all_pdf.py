c22dc4fc7fa109fc1d96ceb87ac96187655dd9eb
dcollins4096 <dcollins4096@gmail.com>
2020-05-20 12:34:21 -0400

    clean up all_pdfs.  add some plots

--- tools_pdf/all_pdf.py
+++ tools_pdf/all_pdf.py
@@ -1,29 +1,64 @@
 
-from starter1 import *
-import yt
-# for new ds.all_data() profile plot - 
-def rs(arr):
-    return arr.reshape(arr.shape + (1,)) 
-def rs2(arr):
-    return arr.reshape(arr.shape + (1,) + (1,)) 
+from starter2 import *
+import matplotlib.image as mpimg
 
-import trackage
-reload(trackage)
-if 'track' not in dir():
-    track = trackage.track_manager(None)
+import data_locations as dl
+reload(dl)
+plt.close('all')
 
-    directory = '/scratch2/dcollins/Paper19_48/B02/u05-r4-l4-128'
-    ds0 = yt.load(directory+"/DD0000/data0000")
-    ad0 = ds0.all_data()
-    extrema = {'density':[5e-3,100]}
-    n_bins=128
+file_list=glob.glob('%s/*h5'%dl.sixteen_frame)
+file_list = glob.glob("/scratch1/dcollins/Paper19/Datasets/all_primitives/*h5")[:10]
+out_prefix = 'CHANGE_THIS_PREFIX'
 
-    #track.read('all_large_0000_0125.h5')
-    #track.read('tracks_first_last_all_nonzero.h5')
-    track.read('tracks_frame012_all_nonzero.h5')
-all_cores = np.unique(track.core_ids)
-if 0:
-    plt.clf()
+out_prefix='test'
+#file_list = file_list[:2]
+
+
+if 'this_looper' not in dir():
+    this_looper=looper.core_looper(directory=dl.enzo_directory)
+    for nfile,fname in enumerate(file_list):
+        this_looper.load_loop(fname)
+        print( "File %d of %d"%(nfile,len(file_list)))
+    thtr = this_looper.tr
+    thtr.sort_time()
+    del dmeans, prof0, prof1, prof_vel
+all_cores = np.unique(thtr.core_ids)
+rm = rainbow_map(len(all_cores))
+
+#all_cores = all_cores[:10]
+def make_prof(ds,fields,weight_field=None,accumulation=False,fractional=True,n_bins=64,extrema=None):
+    reg = ds.all_data()
+    prof = yt.create_profile(reg,fields[0],fields[1] ,weight_field=weight_field,accumulation=accumulation,
+                            fractional=fractional, n_bins=n_bins, extrema=extrema)
+    the_x = 0.5*(prof.x_bins[1:]+prof.x_bins[0:-1])
+    the_y = prof[fields[1]]
+    #if units[0] is not None:
+    #    the_x = the_x.in_units(units[0])
+    #if units[1] is not None and fractional is not True:
+    #    the_y = the_y.in_units(units[1])
+    output={}
+    output['prof']=prof
+    output['the_x']=the_x
+    output['the_y']=the_y
+    return output
+
+plt.clf()
+odir=os.environ['HOME']+'/PigPen/'
+odir = "./plots_to_sort/"
+
+if 'prof0' not in dir():
+    ds0 = yt.load("%s/DD%04d/data%04d"%(dl.enzo_directory,0,0))
+    ds1 = yt.load("%s/DD%04d/data%04d"%(dl.enzo_directory,125,125))
+    prof0=make_prof(ds0,['density','cell_volume'])
+    prof1=make_prof(ds1,['density','cell_volume'])
+
+if 'prof_vel' not in dir():
+    ds = yt.load("%s/DD%04d/data%04d"%(dl.enzo_directory,0,0))
+    prof_vel=make_prof(ds1,['velocity_magnitude','cell_volume'])
+
+#all_cores=all_cores[:5]
+all_densities={}
+if 'dmeans' not in dir():
     dmeans = np.zeros_like(all_cores,dtype='float')
     dstds = np.zeros_like(all_cores,dtype='float')
     logdmeans = np.zeros_like(all_cores,dtype='float')
@@ -32,9 +67,9 @@ if 0:
     vstds = np.zeros_like(all_cores,dtype='float')
     npart = np.zeros_like(all_cores,dtype='float')
     for i,nc in enumerate(all_cores):
-        this_density = track.c(int(nc),'density')[:,0]
+        this_density = thtr.c(int(nc),'density')[:,0]
         npart[i] = this_density.size
-        this_vel = track.c(int(nc),'velocity_magnitude')[:,0]
+        this_vel = thtr.c(int(nc),'velocity_magnitude')[:,0]
         dmeans[i]=this_density.mean()
         dstds[i] = this_density.std()
         logdmeans[i]= np.log(this_density).mean()
@@ -42,66 +77,124 @@ if 0:
         vmeans[i]= this_vel.mean()
         vstds[i] = this_vel.std()
 
-        #plt.hist(np.log10(this_density),histtype='step',color='k')
-        #plt.hist(this_density,histtype='step',color='k')
-    ok=npart>1
-    if 0:
-        plt.clf()
-        plt.scatter(logdstds[ok],vstds[ok])
-        #plt.yscale('log')
-        #plt.xscale('log')
-        #plt.ylabel(r'$\log_{10} ||v||$')
-        plt.ylabel(r'$v_{rms}$')
-        plt.xlabel(r'$\rho_{rms}$')
-        odir='/home/dcollins/RESEARCH2/Paper56_FisherBayesPreimage/2019-03-01-pdfs'
-        plt.savefig(odir+'/pre_logrho_rms_v_rms.png')
-    if 1:
-        plt.scatter(dstds[ok],vstds[ok])
-        plt.yscale('log')
-        plt.xscale('log')
-        #plt.ylabel(r'$\log_{10} ||v||$')
-        plt.ylabel(r'$v_{rms}$')
-        plt.xlabel(r'$\rho_{rms}$')
-        odir='/home/dcollins/RESEARCH2/Paper56_FisherBayesPreimage/2019-03-01-pdfs'
-        plt.savefig(odir+'/pre_rho_rms_v_rms.png')
-    if 1:
-        plt.clf()
-        plt.scatter(dmeans[ok],vstds[ok])
-        plt.yscale('log')
-        plt.xscale('log')
-        #plt.ylabel(r'$\log_{10} ||v||$')
-        plt.ylabel(r'$v_{rms}$')
-        plt.xlabel(r'$\rho$')
-        odir='/home/dcollins/RESEARCH2/Paper56_FisherBayesPreimage/2019-03-01-pdfs'
-        plt.savefig(odir+'/pre_rho_mean_v_rms.png')
-    if 0:
-        plt.clf()
-        plt.scatter(dmeans,vmeans)
-        plt.yscale('log')
-        plt.xscale('log')
-        #plt.ylabel(r'$\log_{10} ||v||$')
-        plt.ylabel(r'$||v||$')
-        plt.xlabel(r'$\rho$')
-        odir='/home/dcollins/RESEARCH2/Paper56_FisherBayesPreimage/2019-03-01-pdfs'
-        plt.savefig(odir+'/pre_rho_v_mean.png')
-if 0:
-    plt.clf()
+if 1:
+    """all the PDFs in one plot"""
+    fig, axes = plt.subplots(1,1)
+    ax1 = axes
+    ax1.plot( prof0['the_x'],prof0['the_y']*128**3,c='k')
+    #ax1.plot( prof1['the_x'],prof1['the_y'],c='k')
     for i,nc in enumerate(all_cores):
-        this_density = track.c(int(nc),'velocity_magnitude')[:,0]
-        #plt.hist(np.log10(this_density),histtype='step',color='k')
-        plt.hist(this_density,histtype='step',color='k')
-    plt.yscale('log')
-    #plt.xlabel(r'$\log_{10} ||v||$')
-    plt.xlabel(r'$||v||$')
-    plt.ylabel(r'$N(v)$')
-    plt.savefig('/home/dcollins4096/PigPen/p56_allpdf_vel_notlog.png')
+        this_density = thtr.c(int(nc),'density')[:,0]
+        if this_density.size > 1:
+            hist_raw, edges =np.histogram(np.log10(this_density),bins=np.linspace(-2,2,16))
+            hhh = hist_raw#/hist_raw.sum()
+            centers = 0.5*(edges[1:]+edges[:-1])
+            ax1.plot( 10**centers, hhh)
+    form = 'pdf'
+    axbonk(ax1,yscale='log',xscale='log', xlabel=r'$\log_{10}\rho$',ylabel=r'$N$')
+    fig.savefig(odir+"density_pdf.%s"%form)
+    plt.close(fig)
+
 if 0:
-    plt.clf()
+    """all the PDFs in one plot"""
+    fig, axes = plt.subplots(1,1)
+    ax1 = axes
+    ax1.plot( prof_vel['the_x'],prof_vel['the_y']*128**3,c='k')
     for i,nc in enumerate(all_cores):
-        this_density = track.c(int(nc),'density')[:,0]
-        plt.hist(np.log10(this_density),histtype='step',color='k')
-    plt.yscale('log')
-    plt.savefig('/home/dcollins4096/PigPen/p47_allhist.png')
+        this_vel = thtr.c(int(nc),'velocity_magnitude')[:,0]
+        if this_vel.size > 1:
+            hist_raw, edges =np.histogram(this_vel)
+            hhh = hist_raw#/hist_raw.sum()
+            centers = 0.5*(edges[1:]+edges[:-1])
+            ok = hhh>0
+            ax1.plot( centers[ok], hhh[ok])
+    form = 'pdf'
+    axbonk(ax1,yscale='log',xscale='log', xlabel=r'$v$',ylabel=r'$N$')
+    fig.savefig(odir+"velocity_pdf.%s"%form)
+ok=npart>1
+fig,ax=plt.subplots()
+
+if 0:
+    """Several different PDFs"""
+    fig, axes = plt.subplots(1,1)
+    ax1 = axes
+    #ax1.plot( prof1['the_x'],prof1['the_y'],c='k')
+    for i,nc in enumerate(all_cores):
+        this_density = thtr.c(int(nc),'density')[:,0]
+        if this_density.size > 1:
+            ax1.clear()
+            ax1.plot( prof0['the_x'],prof0['the_y']*128**3,c='k')
+            hist_raw, edges =np.histogram(np.log10(this_density),bins=np.linspace(-2,2,16))
+            hhh = hist_raw#/hist_raw.sum()
+            centers = 0.5*(edges[1:]+edges[:-1])
+            ax1.plot( 10**centers, hhh)
+            form = 'pdf'
+            axbonk(ax1,yscale='log',xscale='log', xlabel=r'$\log_{10}\rho$',ylabel=r'$N$', ylim=[0.5,1e5])
+            oname = odir+"density_pdf_c%04d.%s"%(nc,form)
+            fig.savefig(oname)
+            print(oname)
+    plt.close(fig)
+
+if 0:
+    if 1:
+        ax.clear()
+        ax.scatter(vmeans[ok],vstds[ok])
+        axbonk(ax,xscale='linear',yscale='linear',xlim=[0,20],ylim=[0,20],xlabel=r'$\langle v\rangle$', ylabel=r'$\sigma_v$')
+        fig.savefig(odir+'/pre_meanv_rms_v.%s'%form)
+    if 1:
+        ax.clear()
+        ax.scatter(logdstds[ok],vstds[ok])
+        #ax.yscale('log')
+        #ax.xscale('log')
+        #ax.ylabel(r'$\log_{10} ||v||$')
+        axbonk(ax,xscale='linear',yscale='linear',xlim=[0,20],ylim=[0,20], xlabel=r'$\sigma_{\ln \rho}$', ylabel=r'$\sigma_v$')
+        fig.savefig(odir+'/pre_logrho_rms_v_rms.%s'%form)
+if 0:
+    if 1:
+        ax.clear()
+        ax.scatter(dstds[ok],vstds[ok])
+        ax.yscale('log')
+        ax.xscale('log')
+        #ax.ylabel(r'$\log_{10} ||v||$')
+        ax.ylabel(r'$\sigma_v$')
+        ax.xlabel(r'$\sigma_{\rho}$')
+        fig.savefig(odir+'/pre_rho_rms_v_rms.%s'%form)
+    if 1:
+        ax.clear()
+        ax.scatter(dmeans[ok],vstds[ok])
+        ax.yscale('log')
+        ax.xscale('log')
+        #ax.ylabel(r'$\log_{10} ||v||$')
+        ax.ylabel(r'$\sigma_v$')
+        ax.xlabel(r'$\langle\rho\rangle$')
+        fig.savefig(odir+'/pre_rho_mean_v_rms.%s'%form)
+    if 1:
+        ax.clear()
+        ax.scatter(dmeans,vmeans)
+        ax.yscale('log')
+        ax.xscale('log')
+        #ax.ylabel(r'$\log_{10} ||v||$')
+        ax.ylabel(r'$\langle v \rangle$')
+        ax.xlabel(r'$\langle \rho \rangle$')
+        fig.savefig(odir+'/pre_rho_v_mean.%s'%form)
+if 0:
+    ax.clear()
+    for i,nc in enumerate(all_cores):
+        this_density = thtr.c(int(nc),'velocity_magnitude')[:,0]
+        #ax.hist(np.log10(this_density),histtype='step',color='k')
+        ax.hist(this_density,histtype='step',color='k')
+    ax.yscale('log')
+    #ax.xlabel(r'$\log_{10} ||v||$')
+    ax.xlabel(r'$||v||$')
+    ax.ylabel(r'$N(v)$')
+    fig.savefig(odir+'/p56_allpdf_vel_notlog.%s'%form)
+if 0:
+    ax.clear()
+    for i,nc in enumerate(all_cores):
+        this_density = thtr.c(int(nc),'density')[:,0]
+        ax.hist(np.log10(this_density),histtype='step',color='k')
+    ax.yscale('log')
+    fig.savefig(odir+'/p47_allhist.png')
 
 
 if 0:
@@ -109,13 +202,13 @@ if 0:
     stds = np.zeros_like(all_cores,dtype='float')
     npart = np.zeros_like(all_cores,dtype='float')
     for i,nc in enumerate(all_cores):
-        this_density = np.log(track.c(int(nc),'density')[:,0])
+        this_density = np.log(thtr.c(int(nc),'density')[:,0])
         means[i]=this_density.mean()
         npart[i] = this_density.size
         stds[i]=np.sqrt(np.sum((this_density-means[i])**2)/npart[i])
-    plt.clf()
-    plt.scatter(means,stds)
-    plt.ylabel(r'$\sqrt{\langle \ln \rho^2 \rangle}$')
-    plt.xlabel(r'$\langle \ln \rho \rangle$')
-    plt.savefig('/home/dcollins4096/PigPen/p47_log_mean_std.png')
+    ax.clear()
+    ax.scatter(means,stds)
+    ax.ylabel(r'$\sqrt{\langle \ln \rho^2 \rangle}$')
+    ax.xlabel(r'$\langle \ln \rho \rangle$')
+    fig.savefig(odir+'/p47_log_mean_std.%s'%form)
 

7f2777c06ecf9c3619f3dba149287f4ffa293b18
dcollins4096 <dcollins4096@gmail.com>
2020-08-10 10:53:56 -0400

    more pdfs

--- p56_plots/all_pdf.py
+++ p56_plots/all_pdf.py
@@ -4,13 +4,16 @@ import matplotlib.image as mpimg
 
 import data_locations as dl
 reload(dl)
+reload(trackage)
 plt.close('all')
 
 file_list=glob.glob('%s/*h5'%dl.sixteen_frame)
 file_list = glob.glob("/scratch1/dcollins/Paper19/Datasets/all_primitives/*h5")
+#file_list = file_list[:20]
 out_prefix = 'CHANGE_THIS_PREFIX'
 
 out_prefix='test'
+form = 'pdf'
 #file_list = file_list[:2]
 
 
@@ -26,6 +29,7 @@ if 'this_looper' not in dir():
 all_cores = np.unique(thtr.core_ids)
 rm = rainbow_map(len(all_cores))
 
+tm = rainbow_map(15)
 #all_cores = all_cores[:10]
 def make_prof(ds,fields,weight_field=None,accumulation=False,fractional=True,n_bins=64,extrema=None):
     reg = ds.all_data()
@@ -59,7 +63,7 @@ if 'prof_vel' not in dir():
 
 #all_cores=all_cores[:5]
 all_densities={}
-if 'dmeans' not in dir():
+if 'dmeans' not in dir() or True:
     dmeans = np.zeros_like(all_cores,dtype='float')
     dstds = np.zeros_like(all_cores,dtype='float')
     logdmeans = np.zeros_like(all_cores,dtype='float')
@@ -82,7 +86,48 @@ if 'dmeans' not in dir():
         vmeans[i]= this_vel.mean()
         vstds[i] = this_vel.std()
 
+
 if 1:
+    #relative and tangential histograms
+    fig, axes=plt.subplots(2,2)
+    axv0 = axes[0][0]
+    axv1 = axes[0][1]
+    axv2 = axes[1][0]
+    axv3 = axes[1][1]
+    #for ni,frame in enumerate(thtr.frames):
+    for ni,frame in enumerate(thtr.frames):
+        for x in axes.flatten():
+            x.clear()
+        for nc,core_id in enumerate(core_list):
+            density = thtr.c([core_id],'density')
+            if density.size < 3:
+                continue
+
+
+            #miniscrubber computes distance, r^2, several other quantities
+            ms = trackage.mini_scrubber(thtr,core_id,do_velocity=True)
+            relative_v = np.sqrt(ms.rel_v2[:,ni])
+            #ax3.clear()
+            ok = (relative_v >0) * ( relative_v > 1e-10)
+            vel_bins = np.logspace(-3,3)
+            nums, bin_edges, rect=axv0.hist(relative_v[ok], histtype='step', color=[0.5]*3, bins=vel_bins)
+            vr = ms.vr_rel[:,ni]
+            vt = np.sqrt( ms.vt2_rel[:,ni])
+            nums, bin_edges, rect=axv1.hist(vr[ok], histtype='step', color=[0.5]*3)
+            nums, bin_edges, rect=axv2.hist(vt[ok], histtype='step', color=[0.5]*3,bins=vel_bins)
+
+            axv3.scatter(vr[ok],vt[ok],c=[ [0.5]*3]*ok.sum(),s=0.1)
+            #ext_x(bin_edges)
+            #ext_y(nums)
+            #oname = 'plots_to_sort/vels_ind_c%04d_n%04d.png'%(core_id, frame)
+        axbonk(axv0,xlabel=r'$\log_{10} v_{rel}$', ylabel=r'$N$',yscale='log',xscale='log')
+        axbonk(axv1,xlabel=r'$v_{r}$', ylabel=r'$N$',xscale='linear',yscale='log', xlim=[-15,15])
+        axbonk(axv2,xlabel=r'$v_{t}$', ylabel=r'$N$',xscale='log',yscale='log')
+        outname = 'plots_to_sort/vel_histograms_n%04d'%frame
+        fig.savefig(outname)
+        print(outname)
+
+if 0:
     fig, axes = plt.subplots(1,1)
     ax1 = axes
     hist_raw, edges =np.histogram(np.log10(volume**(1./3)))
@@ -110,6 +155,69 @@ if 0:
     ax1.set_title('test')
     fig.savefig('plots_to_sort/vels.png')
 
+core_list = this_looper.core_list
+
+
+if 0:
+    #velocity PDFs
+    fig, axes = plt.subplots(1,1)
+    ax1 = axes
+    fig2, axes2 = plt.subplots(1,1)
+    ax2 = axes2 
+    tm = rainbow_map(len(thtr.frames))
+
+    fig3, ax3 = plt.subplots(2,2)
+    ax3.clear()
+    ext_x=extents()
+    ext_x(nar([-2.56e+00, 3.07e+00]))
+
+    ext_y=extents()
+    ext_y(nar([0,4.6e3]))
+    for ni,frame in enumerate(thtr.frames):
+        print(frame)
+        relative_vels=[]
+        ax3.clear()
+        for nc,core_id in enumerate(core_list):
+            density = thtr.c([core_id],'density')
+            if density.size < 3:
+                continue
+
+
+            #miniscrubber computes distance, r^2, several other quantities
+            ms = trackage.mini_scrubber(thtr,core_id,do_velocity=True)
+            relative_v = np.sqrt(ms.rel_v2[:,ni])
+            #ax3.clear()
+            ok = (relative_v >0) * ( relative_v > 1e-10)
+            nums, bin_edges, rect=ax3.hist(np.log(relative_v[ok]), histtype='step', color=[0.5]*3)
+            #ext_x(bin_edges)
+            #ext_y(nums)
+            #oname = 'plots_to_sort/vels_ind_c%04d_n%04d.png'%(core_id, frame)
+            #fig3.savefig(oname)
+            #print(oname)
+            relative_vels += list(  relative_v)
+            print( ni, nc)
+
+
+        bins = np.linspace(0,30,16)
+        hist_raw, edges =np.histogram(thtr.track_dict['velocity_magnitude'][:,ni],bins=bins)
+        hhh = hist_raw#/hist_raw.sum()
+        centers = 0.5*(edges[1:]+edges[:-1])
+        ax1.plot( centers, hhh/hhh.sum(),c=tm(ni),label=ni)
+
+        hist_rel, edges2 =np.histogram(relative_vels,bins=bins)
+        centers2 = 0.5*(edges2[1:]+edges2[:-1])
+        ax2.plot( centers2, hist_rel/hist_rel.sum(),c=tm(ni),label=ni)
+        #axbonk(ax3, xlabel=r'$\log_{10}v_{rel}$', ylabel=r'$N$', xlim=ext_x.minmax, ylim=ext_y.minmax, yscale='log')
+        axbonk(ax3, xlabel=r'$\log_{10}v_{rel}$', ylabel=r'$N$', xlim=[-3,3], ylim=ext_y.minmax, yscale='log')
+        outname='plots_to_sort/vels_ind_n%04d.png'%ni; print(outname)
+        fig3.savefig(outname)
+    axbonk(ax1, xlabel=r'$v$',ylabel=r'$V(v_{total}|*)$')
+    axbonk(ax2, xlabel=r'$v$',ylabel=r'$V(v_{rel}|*)$')
+
+    ax1.legend(loc=1)
+    fig.savefig('plots_to_sort/vels.png')
+    fig2.savefig('plots_to_sort/vels_boosted.png')
+
 if 0:
     """density: all the PDFs in one plot"""
     fig, axes = plt.subplots(1,1)
@@ -123,7 +231,6 @@ if 0:
             hhh = hist_raw#/hist_raw.sum()
             centers = 0.5*(edges[1:]+edges[:-1])
             ax1.plot( 10**centers, hhh)
-    form = 'pdf'
     axbonk(ax1,yscale='log',xscale='log', xlabel=r'$\log_{10}\rho$',ylabel=r'$N$')
     fig.savefig(odir+"density_pdf.%s"%form)
     plt.close(fig)
@@ -177,40 +284,33 @@ if 0:
         fig.savefig(odir+'/pre_meanv_rms_v.%s'%form)
     if 1:
         ax.clear()
-        ax.scatter(logdstds[ok],vstds[ok])
+        ax.scatter(logdstds[ok],vstds[ok],c='k')
         #ax.yscale('log')
         #ax.xscale('log')
         #ax.ylabel(r'$\log_{10} ||v||$')
-        axbonk(ax,xscale='linear',yscale='linear',xlim=[0,20],ylim=[0,20], xlabel=r'$\sigma_{\ln \rho}$', ylabel=r'$\sigma_v$')
+        x = extents()
+        x(logdstds[ok])
+        y = extents()
+        y(vstds[ok])
+        axbonk(ax,xscale='linear',yscale='linear',xlim=x.minmax,ylim=y.minmax, xlabel=r'$\sigma_{\ln \rho}$', ylabel=r'$\sigma_v$')
         fig.savefig(odir+'/pre_logrho_rms_v_rms.%s'%form)
 if 0:
     if 1:
         ax.clear()
         ax.scatter(dstds[ok],vstds[ok])
-        ax.yscale('log')
-        ax.xscale('log')
-        #ax.ylabel(r'$\log_{10} ||v||$')
-        ax.ylabel(r'$\sigma_v$')
-        ax.xlabel(r'$\sigma_{\rho}$')
+        axbonk(ax,yscale='log', xscale='log',  ylabel=r'$\sigma_v$', xlabel=r'$\sigma_{\rho}$')
         fig.savefig(odir+'/pre_rho_rms_v_rms.%s'%form)
     if 1:
         ax.clear()
         ax.scatter(dmeans[ok],vstds[ok])
-        ax.yscale('log')
-        ax.xscale('log')
-        #ax.ylabel(r'$\log_{10} ||v||$')
-        ax.ylabel(r'$\sigma_v$')
-        ax.xlabel(r'$\langle\rho\rangle$')
+        axbonk(ax,yscale='log', xscale='log', ylabel=r'$\sigma_v$', xlabel=r'$\langle\rho\rangle$')
         fig.savefig(odir+'/pre_rho_mean_v_rms.%s'%form)
     if 1:
         ax.clear()
         ax.scatter(dmeans,vmeans)
-        ax.yscale('log')
-        ax.xscale('log')
-        #ax.ylabel(r'$\log_{10} ||v||$')
-        ax.ylabel(r'$\langle v \rangle$')
-        ax.xlabel(r'$\langle \rho \rangle$')
+        axbonk(ax,yscale='log', xscale='log', ylabel=r'$\langle v \rangle$', xlabel=r'$\langle \rho \rangle$')
         fig.savefig(odir+'/pre_rho_v_mean.%s'%form)
+
 if 0:
     ax.clear()
     for i,nc in enumerate(all_cores):
@@ -222,6 +322,7 @@ if 0:
     ax.xlabel(r'$||v||$')
     ax.ylabel(r'$N(v)$')
     fig.savefig(odir+'/p56_allpdf_vel_notlog.%s'%form)
+
 if 0:
     ax.clear()
     for i,nc in enumerate(all_cores):

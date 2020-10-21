7c5f33e706e8b989f55d418ba187385360c044d1
dcollins4096 <dcollins4096@gmail.com>
2020-10-21 13:19:29 -0400

    count particles

--- examples/count_particles.py
+++ examples/count_particles.py
@@ -5,4 +5,4 @@ def count_particles(this_looper,fname='n_particles.txt'):
     for core in this_looper.target_indices:
         fptr.write("%d %d\n"%(core, len( this_looper.target_indices[core])))
     fptr.close()
-count_particles(dp.this_looper,'u10_n_particles.txt')
+#count_particles(this_looper,'u11_n_particles.txt')

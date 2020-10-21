0502bf433dea0b9e995e2b458fb2af6cd2fd6d9e
dcollins4096 <dcollins4096@gmail.com>
2020-10-21 13:20:27 -0400

    refacctor peak getting

--- examples/get_peaks.py
+++ examples/get_peaks.py
@@ -24,4 +24,5 @@ directory = "/archive2/dcollins4096/Paper19/u10_r4_l4_128-Beta2"; frame=82; h5na
 directory = "/archive2/dcollins4096/Paper19/u11_r4_l4_128-Beta20"; frame=88; h5name='u11_n0080_peaks.h5'
 ds = yt.load("%s/DD%04d/data%04d"%(directory,frame,frame))
 
-leaves = loop_tools.get_leaf_indices(ds,small_test=False, h5_name = h5name) #,h5_name="NEW_PEAKS.h5" )
+master_clump = loop_tools.get_leaf_clumps(ds,small_test=False, h5_name = h5name) #,h5_name="NEW_PEAKS.h5" )
+leaves = loop_tools.get_peak_indices(master_clump, ds, h5name) #,h5_name="NEW_PEAKS.h5" )

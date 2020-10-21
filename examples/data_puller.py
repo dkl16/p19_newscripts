aef27a91d3e6b5bd98e6e4f3c1e5cbe899e21630
dcollins4096 <dcollins4096@gmail.com>
2020-10-21 13:20:03 -0400

    minor run differences

--- examples/data_puller.py
+++ examples/data_puller.py
@@ -13,28 +13,32 @@ reload(dl)
 #
 # set sim
 #
-this_simname = 'u10'
+if 'this_simname' not in dir():
+    this_simname = 'u11'
 all_nonzero = looper.get_all_nonzero(dl.n_particles[this_simname])
 
 output_base = "%s_cores"%this_simname
-if 1:
+if 0:
     """this set of parameters extracts all primitive quantities"""
-    core_list = all_nonzero
-    frame_list = [0]#+list(range(10,130,10))+[125]
+    core_list =  all_nonzero
+    frame_list = [0]#[0]#+list(range(10,130,10))+[125]
     fields = ['x','y','z','density','velocity_magnitude','magnetic_field_strength', 'velocity_divergence']
     fields += ['velocity_x','velocity_y','velocity_z']
     fields += ['magnetic_field_%s'%s for s in 'xyz']
+    fields += ['PotentialField']
     output_base = "primitive_test"
     derived=[]
 
-if 0:
+if 1:
     """this set of parameters extracts all primitive quantities"""
-    core_list = all_nonzero.astype('int')
-    frame_list = [0,1]+list(range(10,130,10))+[125]
+    core_list = all_nonzero.astype('int')[::-1]
+    target_frame = dl.target_frames[this_simname]
+    frame_list = [0]+list(range(10,target_frame,10))+[target_frame]
     fields = ['x','y','z','velocity_magnitude','magnetic_field_strength', 'velocity_divergence']
     fields += ['velocity_x','velocity_y','velocity_z']
     fields += ['magnetic_field_%s'%s for s in 'xyz']
-    output_base = "all_primitives"
+    fields += ['PotentialField']
+    output_base = "%s_all_primitives"%this_simname
     derived=[]
 
 if 0:
@@ -45,10 +49,10 @@ if 0:
     derived=[xtra_energy.add_force_terms]
     output_base = 'mag_work_only'
 
-if 0:
+if 1:
     #Pull a whole list for each core, saving each core to its own file
     for core in core_list:  
-        output_name = '%s_c%04d.h5'%(output_base,core)
+        output_name = '%s_primitives_c%04d_nXXX0.h5'%(output_base,core)
         if os.path.exists(output_name):
             print("File exists, skipping "+output_name)
             continue
@@ -66,7 +70,7 @@ if 0:
         this_looper.get_tracks()
         this_looper.save(output_name)
 
-if 1:
+if 0:
     #Pull all cores at once. 
     output_name = '%s_many.h5'%(output_base)
     if not os.path.exists(output_name):
@@ -82,4 +86,4 @@ if 1:
         this_looper.get_target_indices(h5_name=dl.peak_list[this_simname],
                                          bad_particle_list=dl.bad_particles[this_simname])
         this_looper.get_tracks()
-        #this_looper.save(output_name)
+        this_looper.save(output_name)

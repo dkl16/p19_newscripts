408e7f3a24539ad953aef841dd6397f3f167301a
David Collins <dccollins@fsu.edu>
2021-01-15 10:48:19 -0500

    one version works of convex hull

--- tools_tracks/convex_hull.py
+++ tools_tracks/convex_hull.py
@@ -1,119 +1,142 @@
 
 from starter2 import *
 import matplotlib.image as mpimg
-
+from scipy.spatial import ConvexHull
 import matplotlib.patches as patches
 import data_locations as dl
 reload(dl)
 plt.close('all')
 
-#file_list=glob.glob('%s/*h5'%dl.sixteen_frame)
-file_list=glob.glob("../Datasets/u10_primitives_cXXXX_n0000.h5")
-this_simname = 'u10'
-out_prefix = this_simname
 
-#file_list = file_list[:2]
+class hull_tool():
+    def __init__(self,this_looper):
+        self.this_looper=this_looper
+        self.cell_volumes=[]
+        self.hull_volumes=[]
+        self.good_cores=[]
 
-#import tools_tracks.alpha_properties as ap 
-#import p56_plots.all_pdf as ap
-#this_looper=ap.this_looper
-#thtr=this_looper.tr
-#thtr.sort_time()
+    def run(self,do_2d_plots=False,core_list=None):
 
+        thtr = self.this_looper.tr
+        all_cores = np.unique(thtr.core_ids)
+        rm = rainbow_map(len(all_cores))
+        if core_list is None:
+            core_list = all_cores
+        if do_2d_plots:
+            fig_many, ax_many = plt.subplots(2,2,figsize=(8,8))
+            ax_all = [ax_many[0][0], ax_many[0][1], ax_many[1][0]]
+        for core_id in core_list:
+            ms = trackage.mini_scrubber(thtr,core_id)
+            if ms.r.shape[0] <= 4:
+                continue
+            self.good_cores.append(core_id)
+            asort =  np.argsort(thtr.times)
+            delta=0.1
 
-#
-# dear future self:
-#    calling load_loop on the u10 dataset seems to choke.
-#    Reading directly works fine. 
-#
-if 'this_looper' not in dir():
-    this_looper=looper.core_looper(directory=dl.sims[this_simname],savefile=file_list[0])
-    #for nfile,fname in enumerate(file_list):
-    #    this_looper.load_loop(fname)
-    #    print( "File %d of %d"%(nfile,len(file_list)))
-    #thtr = this_looper.tr
-    #thtr.sort_time()
-all_cores = np.unique(thtr.core_ids)
-rm = rainbow_map(len(all_cores))
-core_list = all_cores
-#core_list=[7]
-fig_many, ax_many = plt.subplots(1,1)
-if 'cell_volumes' not in dir():
-    cell_volumes = []
-    hull_volumes = []
-    good_cores=[]
-    for core_id in core_list:
-        ms = trackage.mini_scrubber(thtr,core_id)
-        if ms.r.shape[0] <= 4:
-            continue
-        good_cores.append(core_id)
-        tmap=rainbow_map(ms.ntimes)
-        asort =  np.argsort(thtr.times)
-        delta=0.1
-
-        rrr = ms.r[:,asort]
-        dv = rrr[:,1:] - rrr[:,:-1]
-        mask = slice(None)# dv[:,1]*dv[:,2] < 0
-        for it,nt in enumerate([0]):#asort):
             mask = slice(None)
+            for it,nt in enumerate([0]):#asort):
+
+                this_x,this_y,this_z=ms.this_x[mask,nt],ms.this_y[mask,nt], ms.this_z[mask,nt]
+                this_p = [this_x,this_y,this_z]
+
+                points_3d = np.array(list(zip(this_x,this_y,this_z)))
+                hull_3d = ConvexHull(points_3d)
+                self.hull_volumes.append(hull_3d.volume)
+                self.cell_volumes.append( thtr.c([core_id],'cell_volume')[mask,nt].sum())
 
 
-            ax_many.clear()
-            ax_many.plot([0,1,1,0,0],[0,0,1,1,0])
-            this_frame = sorted(thtr.frames)[nt]
-            cen=this_looper.snaps[this_frame][core_id].R_centroid
-            cenx = cen[0]; ceny=cen[1]
-            #ax_many.scatter(ms.this_x[mask,nt],ms.this_y[mask,nt],s=0.1)
-            #this_x,this_y,this_z=ms.raw_x[mask,nt],ms.raw_y[mask,nt], ms.raw_z[mask,nt]
-            this_x,this_y,this_z=ms.this_x[mask,nt],ms.this_y[mask,nt], ms.this_z[mask,nt]
-            ax_many.scatter(this_x, this_y ,s=0.1)
-            from scipy.spatial import ConvexHull
-            points_3d = np.array(list(zip(this_x,this_y,this_z)))
-            hull_3d = ConvexHull(points_3d)
-            hull_volumes.append(hull_3d.volume)
-            cell_volumes.append( thtr.c([core_id],'cell_volume')[mask,nt].sum())
-            ax_many.plot(points_3d[hull_3d.vertices,0], points_3d[hull_3d.vertices,1], 'r', lw=2)
+                if do_2d_plots:
+                    #for ax in ax_all:
+                    #    ax.clear()
+                    #    ax.set_aspect('equal')
+                    #    ax.plot([0,1,1,0,0],[0,0,1,1,0])
 
-            points_2d = np.array(list(zip(this_x,this_y)))
-            hull_2d = ConvexHull(points_2d)
-            ax_many.plot(points_2d[hull_2d.vertices,0], points_2d[hull_2d.vertices,1], 'k')
-            #ax_many.plot(points[hull.vertices[0],0], points[hull.vertices[0],1], 'ro')
-            #ax_many.scatter( cenx,ceny,c='k',s=3)
-            #ax_many.set_xlim(-delta,1+delta); ax_many.set_ylim(-delta,1+delta)
-            title='t=%0.5f'%thtr.times[nt]
-            ax_many.set_title(title)
-            ax_many.set_aspect('equal')
-            outname = '%s/%s_hull_3d_t_c%04d_n%04d.png'%(dl.output_directory,this_simname,core_id,it)
-            fig_many.savefig(outname)
-            print("Wrote "+outname)
+                    for LOS in [0,1,2]:
+                        x = [0,2,0][LOS]
+                        y = [1,1,2][LOS]
+                        xlab='xyz'[x]
+                        ylab='yzx'[y]
 
-if 1:
-    good_cores=nar(good_cores)
-    hull_volumes=nar(hull_volumes)
-    cell_volumes=nar(cell_volumes)
-    fig,ax=plt.subplots(1,1)
-    hull_lengths = nar(hull_volumes)**(1./3)
-    ax.hist(hull_lengths,histtype='step',color='k')
-    axbonk(ax,xlabel=r'$\rm{Hull\ Length}$',ylabel=r'$\rm{N}$')
-    fig.savefig('plots_to_sort/%s_hull_lengths.pdf'%this_simname)
-    
-    ax.clear()
-    odd = hull_volumes < cell_volumes
-    not_odd = hull_volumes >= cell_volumes
-    ax.scatter(hull_volumes,cell_volumes,c='k')
-    ax.scatter(hull_volumes[odd],cell_volumes[odd],c='r')
-    ax.set_aspect('equal')
-    #axbonk(ax,xlabel=r'$\rm{Hull\ Volume}$',ylabel=r'$\rm{Cell\ Volume}$',xlim=[0,0.07],ylim=[0,0.07])
-    ext=extents()
-    ext(hull_volumes)
-    ext(cell_volumes)
-    axbonk(ax,xlabel=r'$\rm{Hull\ Volume}$',ylabel=r'$\rm{Cell\ Volume}$',xlim=ext.minmax,ylim=ext.minmax,
-          xscale='log',yscale='log')
-    fig.savefig('plots_to_sort/%s_volume_fractions.pdf'%this_simname)
+
+
+                        ax_all[LOS].scatter(this_p[x], this_p[y],s=0.1)
+
+                        points_2d = np.array(list(zip(this_p[x],this_p[y])))
+                        hull_2d = ConvexHull(points_2d)
+                        vert_x = points_2d[hull_2d.vertices,0]
+                        vert_y = points_2d[hull_2d.vertices,1]
+                        vert_x = np.concatenate([vert_x,vert_x[0:1]])
+                        vert_y = np.concatenate([vert_y,vert_y[0:1]])
+                        ax_all[LOS].plot(vert_x, vert_y, 'k')
+
+                        x_min = min([this_p[x].min(), -delta])
+                        x_max = max([this_p[x].max(), 1+delta])
+                        y_min = min([this_p[y].min(), -delta])
+                        y_max = max([this_p[y].max(), 1+delta])
+
+
+                        axbonk(ax_all[LOS],xlabel=xlab,ylabel=ylab,xlim=[x_min,x_max],ylim=[y_min,y_max])
+                        #ax_many.set_title(title)
+                    outname = '%s/%s_hull_3d_t_c%04d_n%04d.png'%(dl.output_directory,self.this_looper.out_prefix,core_id,it)
+                    fig_many.savefig(outname)
+                    print("Wrote "+outname)
+
+import three_loopers as tl
+if 'ht1' not in dir() or clobber:
+    ht1 = hull_tool(tl.looper1)
+    ht1.run(do_2d_plots=True)#,core_list=[10,11])
+if 'ht2' not in dir() or clobber:
+    ht2 = hull_tool(tl.looper2)
+    ht2.run(do_2d_plots=True)
+if 'ht3' not in dir() or clobber:
+    ht3 = hull_tool(tl.looper3)
+    ht3.run(do_2d_plots=True)
 
 if 1:
     import p56_plots.density_AC as AC
-    #reload(AC)
+    reload(AC)
+    if 'a1' not in dir():
+        a1 = AC.ac_thing('u05'); a1.plot()
+        a2 = AC.ac_thing('u10'); a2.plot()
+        a3 = AC.ac_thing('u11'); a3.plot()
+        acs={'u05':a1,'u10':a2,'u11':a3}
+
+if 0:
+    fig,ax=plt.subplots(1,1)
+    for nrun,ht in enumerate([ht1,ht2,ht3]):
+        c='rgb'[nrun]
+        hull_lengths = nar(ht.hull_volumes)**(1./3)
+        vals, bins = np.histogram(hull_lengths)
+        bc = 0.5*(bins[1:]+bins[:-1])
+        ax.plot(bc,vals/vals.sum(),color=c,label=ht.this_looper.out_prefix)
+        axbonk(ax,xlabel=r'$\rm{Hull\ Length}$',ylabel=r'$\rm{N}$')
+
+        ac = acs[ht.this_looper.out_prefix]
+        ax.plot(ac.binned[1],ac.binned[2], c=c)
+    ax.legend(loc=0)
+    fig.savefig('plots_to_sort/hull_lengths.pdf')
+    plt.close('fig')
+    
+
+if 0:
+    fig,ax=plt.subplots(1,1)
+    if 'ext_hull' not in dir():
+        ext_hull=extents()
+    for nrun,ht in enumerate([ht1,ht2,ht3]):
+        odd = nar(ht.hull_volumes) < nar(ht.cell_volumes)
+        not_odd = nar(ht.hull_volumes) >= nar(ht.cell_volumes)
+        ax.scatter(ht.hull_volumes,ht.cell_volumes,c='k')
+        ax.scatter(nar(ht.hull_volumes)[odd],nar(ht.cell_volumes)[odd],c='r')
+        ax.set_aspect('equal')
+        #axbonk(ax,xlabel=r'$\rm{Hull\ Volume}$',ylabel=r'$\rm{Cell\ Volume}$',xlim=[0,0.07],ylim=[0,0.07])
+        ext_hull(nar(ht.hull_volumes))
+        ext_hull(nar(ht.cell_volumes))
+        ax.plot(ext_hull.minmax, ext_hull.minmax,c='g')
+        axbonk(ax,xlabel=r'$\rm{Hull\ Volume}$',ylabel=r'$\rm{Cell\ Volume}$',
+               xlim=ext_hull.minmax,ylim=ext_hull.minmax, xscale='log',yscale='log')
+        fig.savefig('plots_to_sort/%s_volume_fractions.pdf'%ht.this_looper.out_prefix)
+
+if 0:
     fig,ax=plt.subplots(1,1)
     #ax.hist(hull_lengths,histtype='step',color='k',normed=True)
     vals, bins = np.histogram(hull_lengths)

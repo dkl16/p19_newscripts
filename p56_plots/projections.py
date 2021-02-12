72fcbf2fc8b4ded8c1b20fc8b7517d2ecb9e46c5
David Collins <dccollins@fsu.edu>
2021-02-12 15:12:53 -0500

    simplify

--- p56_plots/projections.py
+++ p56_plots/projections.py
@@ -9,71 +9,65 @@ core_list = looper.get_all_nonzero()
 frame_list=[125] #range(0,130,10)
 fields=['density']  
 
-#Look for app_test.h5 and read from that.
-#If not, make a new looper and write app_test.h5.
-if 'this_looper' not in dir():
 
-    directory = '/scratch2/dcollins/Paper19_48/B02/u05-r4-l4-128/GravPotential'
-    this_looper = looper.core_looper(directory= directory, 
-                                     derived=[xtra_energy.add_force_terms], 
-                                     sim_name = 'u05', 
-                                     out_prefix = 'plots_to_sort/u05', 
-                                     target_frame = 125, 
-                                     frame_list = frame_list, 
-                                     core_list = core_list, 
-                                     fields_from_grid=['x','y','z']+fields)
-    this_looper.get_target_indices(h5_name='datasets_small/u05_0125_peaklist.h5',
-                                     bad_particle_list='bad_particles.h5')
-    this_looper.get_tracks()
-
-@looper.frame_loop
-def proj_cores_annotate_zoom(self, axis_list=[0,1,2],core_list=None, center_cores=[],field='density',color='r',
-                            zoom_level=4,cb_label=None):
+def proj_cores_annotate_zoom(this_looper, axis_list=[0,1,2],core_list=None, center_cores=[],field='density',color='r',
+                            zoom_level=4,cb_label=None, frame_list=None,plot_dir="./plots_to_sort"):
     """Full projections of the data, with core particles marked."""
+    if frame_list is None:
+        frame_list = [this_looper.target_frame]
     if core_list is None:
-        core_list = list(self.target_indices.keys())
-    for snapshot in self.snaps[self.current_frame].values():
-        if snapshot.R_centroid is None:
-            snapshot.get_all_properties()
-    for axis in axis_list:
-        ds = self.ds_list[self.current_frame]
-        proj_center = ds.arr([0.6,0.6,0.6], 'code_length')
+        core_list = list(this_looper.target_indices.keys())
+    if core_list == "short":
+        core_list = list(this_looper.target_indices.keys())[:5]
+    if center_cores == None:
+        center_cores = core_list
+    if center_cores == "one":
+        center_cores = core_list[0:1]
+    for frame in frame_list:
+        print("%s %d"%(this_looper.out_prefix,frame))
+        for snapshot in this_looper.snaps[frame].values():
+            if snapshot.R_centroid is None:
+                snapshot.get_all_properties()
+        for axis in axis_list:
+            ds = this_looper.load(frame)
+            center = this_looper.ds.arr(nar([0.5]*3),'code_length')
+            #this_looper.proj = yt.ProjectionPlot(this_looper.ds, axis=axis, fields=[field])#, center=center)
+            proj = ds.proj(field,axis,center=center)
+            this_looper.proj=proj.to_pw(width=(1.0,'code_length'),center=center,origin='domain')
+            #this_looper.proj.set_cmap(field,'Greys')
+            this_looper.proj.set_cmap(field,'Greys')
+            this_looper.proj.zoom(zoom_level)
+            this_looper.proj.set_axes_unit('code_length')
+            for nc,core_number in enumerate(core_list):
+                snapshot = this_looper.snaps[frame][core_number]
+                center = ds.arr(snapshot.R_centroid,'code_length')
+                this_looper.proj.annotate_text(loop_apps.cbump(center),
+                                 ".",text_args={'color':color}, 
+                                 inset_box_args={'visible':False},
+                                 coord_system='data')
+                this_looper.proj.annotate_text(loop_apps.cbump(center),
+                                 "%d"%snapshot.core_id,text_args={'color':color}, 
+                                 inset_box_args={'visible':False},
+                                 coord_system='data')
 
-        center = self.ds.arr(nar([0.5]*3),'code_length')
-        print("poot")
-        #self.proj = yt.ProjectionPlot(self.ds, axis=axis, fields=[field])#, center=center)
-        proj = ds.proj(field,axis,center=center)
-        self.proj=proj.to_pw(width=(1.0,'code_length'),center=center,origin='domain')
-        #self.proj.set_cmap(field,'Greys')
-        self.proj.set_cmap(field,'Greys')
-        self.proj.zoom(zoom_level)
-        self.proj.set_axes_unit('code_length')
-        for nc,core_number in enumerate(core_list):
-            ms = trackage.mini_scrubber(self.tr,core_number)
-            snapshot = self.snaps[self.current_frame][core_number]
-            center = ds.arr(snapshot.R_centroid,'code_length')
-            print("Core %d center %s"%(core_number, str(center)))
-            #self.proj.annotate_select_particles4(1.0, col='r', indices=self.target_indices[core_number])
-            self.proj.annotate_text(loop_apps.cbump(center),
-                             "%d"%snapshot.core_id,text_args={'color':color}, 
-                             inset_box_args={'visible':False},
-                             coord_system='data')
+            for core_number in center_cores:
+                snapshot = this_looper.snaps[frame][core_number]
+                center = ds.arr(snapshot.R_centroid,'code_length')
+                plot_x = [1,2,0][axis]
+                plot_y = [2,0,1][axis]
+                this_center = center[plot_x],center[plot_y]
+                outname = '%s/%s_core_zoom_annotate_c%04d_n%04d'%(plot_dir,this_looper.out_prefix,core_number,frame)
+                if cb_label is not None:
+                    this_looper.proj.set_colorbar_label(field,cb_label)
+                this_looper.proj.set_center(this_center)
+                print( this_looper.proj.save(outname))
 
-        for core_number in center_cores:
-            snapshot = self.snaps[self.current_frame][core_number]
-            center = ds.arr(snapshot.R_centroid,'code_length')
-            plot_x = [1,2,0][axis]
-            plot_y = [2,0,1][axis]
-            this_center = center[plot_x],center[plot_y]
-            outname = '%s_core_zoom_annotate_c%04d_n%04d'%(self.out_prefix,core_number,self.current_frame)
-            if cb_label is not None:
-                self.proj.set_colorbar_label(field,cb_label)
-            self.proj.set_center(this_center)
-            print( self.proj.save(outname))
-
+import three_loopers as tl
 if 1:
 
-    proj_cores_annotate_zoom(this_looper,axis_list=[0],core_list=core_list,center_cores=[8],cb_label='density')
+    for this_looper in [tl.looper1,tl.looper2,tl.looper3]:
+        proj_cores_annotate_zoom(this_looper,axis_list=[0],core_list=None,center_cores=None,cb_label='density',
+                                plot_dir="./plots_to_sort_2")
 
 if 0:
     proj_cores_annotate_zoom(this_looper,axis_list=[0],core_list=core_list,center_cores=[34],cb_label='density',

81d79eee2e16f9c8512f0984fc5b73f1e71715dc
David Collins <dccollins@fsu.edu>
2021-04-21 15:11:33 -0400

    good mass plot.

--- tools_tracks/mass_time.py
+++ tools_tracks/mass_time.py
@@ -12,6 +12,7 @@ class mass_tool():
     def __init__(self,this_looper):
         self.this_looper=this_looper
         self.mass=defaultdict(list)
+        self.dof =defaultdict(list)
         self.cores_used=[]
     def run(self,core_list=None):
         dx=1./2048
@@ -28,29 +29,31 @@ class mass_tool():
         self.core_list=core_list
         for core_id in core_list:
             ms = trackage.mini_scrubber(thtr,core_id)
-            if ms.nparticles < 3:
+            if ms.nparticles < 10:
                 continue
             print('go ', core_id)
             self.cores_used.append(core_id)
             self.times = thtr.times
 
             for nf,frame in enumerate(thtr.frames):
-                x =np.floor(thtr.c([core_id],'x')/dx)[:,nf]#or whatever the number of zones is
-                y =np.floor(thtr.c([core_id],'y')/dx)[:,nf]
-                z =np.floor(thtr.c([core_id],'z')/dx)[:,nf]
+                ix =np.floor(thtr.c([core_id],'x')/dx)[:,nf]
+                iy =np.floor(thtr.c([core_id],'y')/dx)[:,nf]
+                iz =np.floor(thtr.c([core_id],'z')/dx)[:,nf]
                 density = thtr.c([core_id],'density')[:,nf]
                 cell_volume = thtr.c([core_id],'cell_volume')[:,nf]
-                index = x + nx*(y * nx*z)
+                index = ix + nx*(iy * nx*iz)
                 ar = np.argsort(index)
                 rs = np.argsort(ar)
                 isorted=index[ar]
                 mask = np.ones_like(density,dtype='bool')
                 mask[1:] = isorted[1:]-isorted[:-1] != 0
                 mask2 = mask[ rs]
-                mass = (density[mask2]*cell_volume[mask2]).sum()
+                #mass = (density[mask2]*cell_volume[mask2]).sum()
+                mass = (density*cell_volume).sum()
                 self.mass[core_id].append(mass)
+                self.dof[core_id].append( mask2.sum())
 
-import three_loopers as tl
+import three_loopers_1tff as tl
 if 'clobber' not in dir():
     clobber=True
 if 'mass_tool1' not in dir() or clobber:
@@ -64,32 +67,51 @@ if 'mass_tool3' not in dir() or clobber:
     mass_tool3=mass_tool(tl.looper3)
     mass_tool3.run()
 
-fig,ax=plt.subplots(2,2)
+fig,ax=plt.subplots(1,3, figsize=(12,4))
 axes=ax.flatten()
 if 1:
     for nt,tool in enumerate([mass_tool1,mass_tool2,mass_tool3]):
+        G=1620./(4*np.pi)
+        tff_global = np.sqrt(3*np.pi/(32*G*1))
+        ntimes = tool.times.size
+        ncores = len(tool.cores_used)
+        masses = np.zeros([ntimes,ncores])
+        these_times = tool.times/tff_global
+        for ncore,core_id in enumerate(tool.cores_used):
+            masses[:,ncore]=tool.mass[core_id]/nar(tool.mass[core_id][:6]).mean()
 
+        nc = len(tool.cores_used)
+        take_a_few = ((nc-1)*np.random.random(10)).astype('int')
+        print(take_a_few)
+        for ncore,core_id in enumerate(nar(tool.cores_used)[take_a_few]):
+            #rel_mass = (tool.mass[core_id]-tool.mass[core_id][0])/tool.mass[core_id][0]
+            #rel_mass=tool.mass[core_id]/nrm#/tool.mass[core_id][-1]
+            rel_mass=masses[:,ncore]
 
+            axes[nt].plot(these_times, rel_mass,c=[0.5]*4)
+            #axes[3].plot(these_times, tool.dof[core_id]/tool.dof[core_id][-1],c=[0.5]*4)
 
-        for core_id in [9]: #tool.cores_used:
-            rel_mass = (tool.mass[core_id]-tool.mass[core_id][0])/tool.mass[core_id][0]
-            if rel_mass[8] > 0:
-                print("got one", core_id)
-                print(tool.this_looper.tr.c([core_id],'density').shape)
+        mass_bins_edge = np.logspace(-3,4,101)
+        xbins = these_times
+        ybins = 0.5*(mass_bins_edge[1:]+mass_bins_edge[:-1])
+        nx = len(xbins) ; ny=len(ybins)
+        TheX = np.r_[(ny)*[xbins]].transpose()
+        TheY = np.r_[(nx)*[ybins]]
 
+        hist = np.zeros( [xbins.size,ybins.size])
+        for ntime, time in enumerate(these_times):
+            thishist,bins = np.histogram(masses[ntime,:],bins=mass_bins_edge)
+            hist[ntime,:]=thishist
 
-            axes[nt].plot(tool.times, rel_mass,c=[0.5]*4)
-        axes[nt].set_yscale('symlog',linthresh=1)
-
-    fig.savefig('plots_to_sort/mass_time.pdf')
-
-if 0:
-    for nt,tool in enumerate([mass_tool1]):
-        rel_mass = (1-tool.mass[core_id]/tool.mass[core_id][0])
-
-        for core_id in tool.cores_used[4::10]:
-            axes[nt].plot(tool.times,x  ,c=[0.5]*4)
-        axes[nt].set_yscale('symlog',linthresh=1)
-
-    fig.savefig('plots_to_sort/mass_time.pdf')
+        cmap = copy.copy(mpl.cm.get_cmap("viridis"))
+        cmap.set_under('w')
+        minmin = hist[hist>0].min()
+        norm = mpl.colors.LogNorm(vmin=1,vmax=33)
+        ploot=axes[nt].pcolormesh(TheX, TheY, hist, cmap=cmap,norm=norm,shading='nearest')
+        axes[nt].plot(these_times, [2]*tool.times.size,c='r')#,lw=0.2)
+        axes[nt].plot(these_times, [1./2]*tool.times.size,c='r')#,lw=0.2)
+        axbonk(axes[nt],ylabel=None,xlabel=r'$t/t_{\rm{ff}}$',yscale='log')
+    axes[0].set_ylabel(r'$\rho/\bar{\rho_{\rm{early}}}$')
+    fig.colorbar(ploot)
+    fig.savefig('plots_to_sort/mass_time_heatmap.pdf')
 
